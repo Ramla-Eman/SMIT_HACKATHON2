34c47f3017d47e1224376ed8b6bbc7e7cdd6a0ef
Ramla-Eman <ramla.eman.ra@gmail.com>
2025-07-27 12:28:15 +0500

    working on deployemnet

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    console.log('Attempting to connect to MongoDB...');
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('MongoDB Connection Error:', error);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
